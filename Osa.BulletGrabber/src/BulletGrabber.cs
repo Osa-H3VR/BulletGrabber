cd4e53106620592caab3dea64d9dacc5d448403e
OsaPL <osa12pl@gmail.com>
2021-06-02 00:08:38 +0200

    I think it works

---
+++ Osa.BulletGrabber/src/BulletGrabber.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Deli.Immediate;
+using Deli.Setup;
+using Deli.VFS;
+using Deli.VFS.Disk;
+using UnityEngine.SceneManagement;
+
+namespace Osa.BulletGrabber
+{
+    // DeliBehaviours are just MonoBehaviours that get added to a global game object when the game first starts.
+    public class BulletGrabber : DeliBehaviour
+    {
+        private Hooks _hooks;
+        
+        // All Deli properties can be accessed here, but don't use Unity's API until Awake.
+        public BulletGrabber()
+        {
+            // There is 1 log message in each of the methods here.
+            // Run this mod to see the order of execution logged to console.
+            Logger.LogInfo("BulletGrabber loading...");
+
+            // Hook to the setup stage (the first stage we can access)
+            // Do not forget! Awake is still ran before ANY event.
+            Stages.Setup += OnSetup;
+
+            // Check if the mod is on disk (as opposed to a zip file)
+            if (Resources is IDiskHandle resourcesOnDisk)
+            {
+                Logger.LogInfo($"The mod is on disk at: '{resourcesOnDisk.PathOnDisk}'");
+                
+                // Every time the scene changes, reload our resources to see if they changed.
+                SceneManager.activeSceneChanged += (x, p) => resourcesOnDisk.Refresh();
+            }
+        }
+
+        // Now you can use Unity's API
+        private void Awake()
+        {
+            LoadCfg();
+        }
+        
+        // The config system is identical to BepInEx, but here's a demo if you are unfamiliar.
+        // They are good with simple types (primitives and small structs), but for larger sets of data you may want to
+        // use an asset loader of some sort.
+        private void LoadCfg()
+        {
+            //"localhost", 16834
+            var hand = Config.Bind("BulletGrabber", "HandMode", "", "Defines which hands can grab bullets. Valid options are: 'both', 'left' or 'right'.");
+            var delay = Config.Bind("BulletGrabber", "Delay", 1000, "Defines delay (in milliseconds) between each bullet grab. To be safe, pick something higher than 200.");
+            var range = Config.Bind("BulletGrabber", "Range", 3.2f, "Defines range (in meters) of grab. Setting it higher than 3.2 will disable TnH scoring.");
+            
+            Logger.LogInfo($"Loaded in: '{hand.Value}' hand(s) mode");
+            Logger.LogInfo($"Loaded in: '{delay.Value}' ms of delay");
+            Logger.LogInfo($"Loaded in: '{range.Value}' range");
+            
+            _hooks = new Hooks(delay.Value,range.Value, Logger);
+        }
+        
+        // And now you can access much more of Deli
+        private void OnSetup(SetupStage stage)
+        {
+
+        }
+
+        private void ConstructTextResource(SetupStage stage)
+        {
+            
+        }
+        
+        private void OnDestroy()
+        {
+            _hooks?.Dispose();
+        }
+
+        private void Start()
+        {
+            Logger.LogInfo("BulletGrabber loaded.");
+        }
+    }
+}
---
+++ Osa.BulletGrabber/src/Hooks.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using BepInEx.Logging;
+using Deli.Setup;
+using On.FistVR;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using FVRPhysicalObject = FistVR.FVRPhysicalObject;
+using FVRViveHand = FistVR.FVRViveHand;
+
+namespace Osa.BulletGrabber
+{
+    public class Hooks
+    {
+        private readonly int _delay;
+        private readonly float _range;
+        private readonly ManualLogSource _manualLogSource;
+
+        public Hooks(int delay, float range, ManualLogSource manualLogSource)
+        {
+            _delay = delay;
+            _range = range;
+            _manualLogSource = manualLogSource;
+            Hook();
+        }
+
+        public void Dispose()
+        {
+            Unhook();
+        }
+
+        private void Unhook()
+        {
+        }
+
+        private void Hook()
+        {
+            On.FistVR.FVRViveHand.RetrieveObject += FVRViveHandOnRetrieveObject;
+            On.FistVR.FVRFireArmRound.UpdateInteraction += FVRFireArmRoundOnUpdateInteraction;
+
+            if (_range > 3.2)
+            {
+                // TODO! Disable scoring when more than 3.2
+            }
+        }
+
+        private void FVRFireArmRoundOnUpdateInteraction(FVRFireArmRound.orig_UpdateInteraction orig,
+            FistVR.FVRFireArmRound self, FVRViveHand hand)
+        {
+            orig(self, hand);
+            // Check if grabber is active
+            if (_active)
+            {
+                if (self.HoveredOverRound == null)
+                    return;
+
+                _manualLogSource.LogInfo("Palming the round");
+                self.PalmRound(self.HoveredOverRound, false, true);
+            }
+        }
+
+        private void FVRViveHandOnRetrieveObject(On.FistVR.FVRViveHand.orig_RetrieveObject orig,
+            FistVR.FVRViveHand self, FVRPhysicalObject obj)
+        {
+            orig(self, obj);
+            _manualLogSource.LogInfo($"Called");
+            if (obj is FistVR.FVRFireArmRound round)
+            {
+                _manualLogSource.LogInfo($"Is round!");
+                
+                if (!round.isPalmable || round.MaxPalmedAmount <= 1)
+                    return;
+                
+                FistVR.FVRFireArmRound[] array = UnityEngine.Object.FindObjectsOfType<FistVR.FVRFireArmRound>();
+                
+                _manualLogSource.LogInfo($"Found {array.Length} rounds");
+                if (array.Length >= round.MaxPalmedAmount - 1)
+                {
+                    _manualLogSource.LogWarning($"Reached palm limit of {round.MaxPalmedAmount}!");
+                }
+                
+                // Filter out bullets
+                SortedList<float, FistVR.FVRFireArmRound> pickupAble = new SortedList<float, FistVR.FVRFireArmRound>();
+                foreach (FistVR.FVRFireArmRound bulet in array)
+                {
+                    _manualLogSource.LogInfo($"Round type {bulet.RoundType} vs {round.RoundType}");
+                    // Only find compatible and unspent ammo
+                    if (bulet.RoundType == round.RoundType && !bulet.IsSpent)
+                    {
+                        _manualLogSource.LogInfo($"Round type is compatible");
+                        // Check for rounds you dont want to select
+                        if (!bulet.IsHeld && bulet.QuickbeltSlot == null)
+                        {
+                            // Check the distance
+                            var distance = Vector3.Distance(round.Transform.position, bulet.Transform.position);
+                            _manualLogSource.LogInfo($"Too far!");
+                            if (distance < _range){
+                                _manualLogSource.LogInfo($"Adding to the list");
+                                pickupAble.Add(distance, bulet);
+                            }
+                        }
+                    }
+                }
+
+                _manualLogSource.LogInfo($"Adding to palm {pickupAble.Count} rounds");
+                _lastCall = DateTime.Now;
+                AnvilManager.Run(GetBullets(pickupAble.Take(round.MaxPalmedAmount - 1).Select(x=>x.Value).ToList(), round));
+            }
+        }
+
+        private DateTime _lastCall;
+        private bool _active;
+
+        private IEnumerator GetBullets(List<FistVR.FVRFireArmRound> list, FistVR.FVRFireArmRound round)
+        {
+            _active = true;
+
+            foreach (FistVR.FVRFireArmRound armRound in list)
+            {
+                if (_lastCall + TimeSpan.FromMilliseconds(_delay) < DateTime.Now)
+                {
+                    // Wait more
+                    yield return null;
+                }
+                else
+                {
+                    _lastCall = DateTime.Now;
+
+                    round.HoveredOverRound = armRound;
+                    yield return null;
+                }
+            }
+
+            _active = false;
+        }
+    }
+}
